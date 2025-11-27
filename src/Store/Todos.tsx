3e80b7820b972cc6b4072be998ff5b01869989b7
DilipLovevanshi <dilip@aperasoftwares.com>
2025-11-27 11:40:17 +0530

    remove some error while present on ReactNode

--- src/Store/Todos.tsx
+++ src/Store/Todos.tsx
@@ -1,4 +1,6 @@
-import { ReactNode, createContext, useContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
+import type { ReactNode } from 'react';
+
 
 export type TodosProviderProps = {
     children: ReactNode
